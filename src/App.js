32b10639940c74500ce2c1c452885d6d905d572d
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-09-02 01:02:50 +0600

    design header , added routing path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import CreatePost from "./pages/CreatePost";
+import Explore from "./pages/Explore ";
+import ForgetPassword from "./pages/ForgetPassword";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Explore />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/forget-password" element={<ForgetPassword />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create" element={<CreatePost />} />
+          </Routes>
+        </main>
+        {/* <Footer /> */}
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
--- src/pages/Explore .js
+++ src/pages/Explore .js
@@ -1,5 +1,13 @@
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">left navbar</div>
+        <div class="col-9">Main Blog</div>
+        <div class="col">right navbar</div>
+      </div>
+    </div>
+  );
 };
 
 export default Explore;
